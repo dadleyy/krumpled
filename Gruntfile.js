86ed2aef27e6e17238851de74e28bd066fca6820
Danny Hadley <dadley@sizethreestudios.com>
2014-05-14 23:08:17 -0400

    started work on sass assets

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,19 @@
 module.exports = function(grunt) {
 
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
   grunt.initConfig({
+
+    sass: {
+      build: {
+        options: {
+          loadPath: require('node-neat').includePaths
+        },
+        files: {
+          'public/css/app.css': 'assets/sass/app.sass'
+        }
+      }
+    }
 
   });
 
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <title>krumpled</title>
+  <link href="/css/app.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
   @yield('content')
